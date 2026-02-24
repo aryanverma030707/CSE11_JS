7bff0d2625feb2d122eff37dd6fe461179efdde9
Aryan.1181 <aryanverma030707@gmail.com>
2026-02-24 10:30:56 +0530

    Add initial HTML and JavaScript files with basic loop and arithmetic operations

---
+++ script1.js
@@ -0,0 +1,11 @@
+let x = 10, y = 3;
+
+console.log(x + y);  // 13
+console.log(x - y);  // 7
+console.log(x * y);  // 30
+console.log(x / y);  // 3.333...
+console.log(x % y);  // 1
+
+let z = 5;
+console.log(++z);    // 6
+console.log(--z);    // 5
