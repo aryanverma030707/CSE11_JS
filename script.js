e88fa002268c040abcd4c913be7bb3cddfd1524a
Aryan.1181 <aryanverma030707@gmail.com>
2026-02-24 09:44:11 +0530

    Refactor HTML structure and enhance JavaScript functionality with variable type checks and symbol comparison

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("Hello, World!");
-document.write("Welcome to JavaScript programming.");
+//console.log("Hello, World!");
+//document.write("Welcome to JavaScript programming.");node
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+//
+
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if (sym1===sym2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
